a6d55548ba3ff39214dbf7da7758e6a0b64f5154
Chris Acheson <mail@chrisacheson.net>
2017-03-16 17:16:50 -0400

    [extraordinary layout] Don't swap thumb clusters
    Don't swap thumb cluster keys on the symbol layer

--- keyboards/ergodox/keymaps/extraordinary/keymap.c
+++ keyboards/ergodox/keymaps/extraordinary/keymap.c
@@ -93,11 +93,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *     | LCtrl | Meh  |Hyper | LAlt | LGui |                                   |   0  |   .  |   =  |   +  |  Ent  |
  *     `-----------------------------------'                                   `-----------------------------------'
  *                                         ,-------------.       ,-------------.
- *                                         | Left | Right|       | Home | End  |
+ *                                         | Home | End  |       | Left | Right|
  *                                  ,------|------|------|       |------+------+------.
- *                                  |      |      |  Up  |       | PgUp |      |      |
- *                                  |Space |Enter |------|       |------|BackSp| Del  |
- *                                  |      |      | Down |       | PgDn |      |      |
+ *                                  |      |      | PgUp |       |  Up  |      |      |
+ *                                  |Backsp| Del  |------|       |------| Enter| Space|
+ *                                  |      |      | PgDn |       | Down |      |      |
  *                                  `--------------------'       `--------------------'
  */
 [SYMB] = KEYMAP(
@@ -105,20 +105,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_TRNS ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_ESC
 ,KC_TRNS ,KC_EXLM ,KC_AT   ,KC_LCBR ,KC_RCBR ,KC_AMPR ,LSFT(KC_COMM)
 ,KC_TRNS ,KC_HASH ,KC_DLR  ,KC_LPRN ,KC_RPRN ,KC_GRV
-,KC_TRNS ,KC_PERC ,KC_CIRC ,KC_LBRC ,KC_RBRC ,KC_TILD ,KC_TAB
+,KC_TRNS ,KC_PERC ,KC_CIRC ,KC_LBRC ,KC_RBRC ,KC_TILD ,LSFT(KC_TAB)
 ,KC_LCTL ,KC_MEH  ,KC_HYPR ,KC_LALT ,KC_LGUI
-                                             ,KC_LEFT ,KC_RGHT
-                                                      ,KC_UP
-                                    ,KC_SPC  ,KC_ENT  ,KC_DOWN
+                                             ,KC_HOME ,KC_END
+                                                      ,KC_PGUP
+                                    ,KC_BSPC ,KC_DEL  ,KC_PGDN
                                                                  // right hand
                                                                  ,KC_MINS     ,KC_F6   ,KC_F7 ,KC_F8  ,KC_F9 ,KC_F10  ,KC_TRNS
                                                                  ,LSFT(KC_DOT),KC_PIPE ,KC_7  ,KC_8   ,KC_9  ,KC_SLSH ,KC_TRNS
                                                                               ,KC_SLSH ,KC_4  ,KC_5   ,KC_6  ,KC_ASTR ,KC_TRNS
-                                                                 ,LSFT(KC_TAB),KC_BSLS ,KC_1  ,KC_2   ,KC_3  ,KC_MINS ,KC_TRNS
+                                                                 ,KC_TAB      ,KC_BSLS ,KC_1  ,KC_2   ,KC_3  ,KC_MINS ,KC_TRNS
                                                                                        ,KC_0  ,KC_DOT ,KC_EQL,KC_PLUS ,KC_ENT
-                                                                 ,KC_HOME     ,KC_END
-                                                                 ,KC_PGUP
-                                                                 ,KC_PGDN     ,KC_BSPC ,KC_DEL
+                                                                 ,KC_LEFT     ,KC_RGHT
+                                                                 ,KC_UP
+                                                                 ,KC_DOWN     ,KC_ENT  ,KC_SPC
 ),
 
 /******* Media Layer *******************************************************************************************************
